c96c20139512a5fd5b8c5beb27079340a508c760
Biswajit Jana <51967298+BlackBull-Rider@users.noreply.github.com>
2026-06-18 21:31:06 +0530

    Create portfolio.py

---
+++ core/portfolio.py
@@ -0,0 +1,270 @@
+# core/portfolio.py
+
+import pandas as pd
+from datetime import datetime
+
+from database.db import get_connection
+
+
+# ==========================
+# BUY STOCK
+# ==========================
+
+def buy_stock(
+    symbol,
+    qty,
+    price,
+    charges=0
+):
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    invested = (
+        qty * price
+    ) + charges
+
+    existing = cur.execute(
+        """
+        SELECT *
+        FROM portfolio
+        WHERE symbol=?
+        """,
+        (symbol,)
+    ).fetchone()
+
+    if existing:
+
+        old_qty = float(
+            existing["qty"]
+        )
+
+        old_avg = float(
+            existing["avg_price"]
+        )
+
+        new_qty = (
+            old_qty + qty
+        )
+
+        new_avg = (
+
+            (old_qty * old_avg)
+
+            +
+
+            (qty * price)
+
+        ) / new_qty
+
+        cur.execute(
+        """
+        UPDATE portfolio
+        SET qty=?,
+            avg_price=?,
+            invested=?
+        WHERE symbol=?
+        """,
+        (
+            new_qty,
+            new_avg,
+            new_qty * new_avg,
+            symbol
+        ))
+
+    else:
+
+        cur.execute(
+        """
+        INSERT INTO portfolio(
+
+            symbol,
+            qty,
+            avg_price,
+            invested,
+            current_value,
+            pnl,
+            pnl_percent,
+            updated_at
+
+        )
+
+        VALUES(
+            ?,?,?,?,?,?,?,?
+        )
+        """,
+        (
+            symbol,
+            qty,
+            price,
+            invested,
+            invested,
+            0,
+            0,
+            datetime.now().isoformat()
+        ))
+
+    cur.execute(
+    """
+    INSERT INTO transactions(
+
+        symbol,
+        txn_type,
+        qty,
+        price,
+        charges,
+        txn_date
+
+    )
+
+    VALUES(
+        ?,?,?,?,?,?
+    )
+    """,
+    (
+        symbol,
+        "BUY",
+        qty,
+        price,
+        charges,
+        datetime.now().isoformat()
+    ))
+
+    conn.commit()
+    conn.close()
+
+
+# ==========================
+# SELL STOCK
+# ==========================
+
+def sell_stock(
+    symbol,
+    qty,
+    price,
+    charges=0
+):
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    row = cur.execute(
+        """
+        SELECT *
+        FROM portfolio
+        WHERE symbol=?
+        """,
+        (symbol,)
+    ).fetchone()
+
+    if not row:
+
+        conn.close()
+        return
+
+    current_qty = float(
+        row["qty"]
+    )
+
+    new_qty = (
+        current_qty - qty
+    )
+
+    if new_qty <= 0:
+
+        cur.execute(
+            """
+            DELETE FROM portfolio
+            WHERE symbol=?
+            """,
+            (symbol,)
+        )
+
+    else:
+
+        cur.execute(
+        """
+        UPDATE portfolio
+        SET qty=?
+        WHERE symbol=?
+        """,
+        (
+            new_qty,
+            symbol
+        ))
+
+    cur.execute(
+    """
+    INSERT INTO transactions(
+
+        symbol,
+        txn_type,
+        qty,
+        price,
+        charges,
+        txn_date
+
+    )
+
+    VALUES(
+        ?,?,?,?,?,?
+    )
+    """,
+    (
+        symbol,
+        "SELL",
+        qty,
+        price,
+        charges,
+        datetime.now().isoformat()
+    ))
+
+    conn.commit()
+    conn.close()
+
+
+# ==========================
+# PORTFOLIO TABLE
+# ==========================
+
+def get_portfolio():
+
+    conn = get_connection()
+
+    query = """
+    SELECT *
+    FROM portfolio
+    """
+
+    df = pd.read_sql_query(
+        query,
+        conn
+    )
+
+    conn.close()
+
+    return df
+
+
+# ==========================
+# TRANSACTION HISTORY
+# ==========================
+
+def get_transactions():
+
+    conn = get_connection()
+
+    query = """
+    SELECT *
+    FROM transactions
+    ORDER BY txn_date DESC
+    """
+
+    df = pd.read_sql_query(
+        query,
+        conn
+    )
+
+    conn.close()
+
+    return df
